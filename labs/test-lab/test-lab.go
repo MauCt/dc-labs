2f408e45b819d73c352dbbff1dce502469adaf20
Mauricio Castro Torres <maucastrotorres97@gmail.com>
2021-01-26 02:54:00 +0000

    Solve test-lab
    Signed-off-by: Mauricio Castro Torres <maucastrotorres97@gmail.com>

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Welcome to the jungle")
+	name := ""
+	//_ es para ignorar el indice en el for
+
+	if len(os.Args) > 1 {
+		for _, word := range os.Args[1:] {
+			name = fmt.Sprint(name, " ", word)
+
+		}
+		fmt.Println("Welcome to the jungle", name, "!")
+	} else {
+		fmt.Println("Error! No name!")
+	}
+
 }
