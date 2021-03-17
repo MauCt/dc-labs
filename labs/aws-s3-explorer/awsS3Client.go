70b84cacb2b67688f852bb18b517781e29c285b4
Mauricio Castro Torres <maucastrotorres97@gmail.com>
2021-03-17 03:15:56 +0000

    Solucion 1
    Signed-off-by: Mauricio Castro Torres <maucastrotorres97@gmail.com>

--- labs/aws-s3-explorer/awsS3Client.go
+++ labs/aws-s3-explorer/awsS3Client.go
@@ -1,4 +1,34 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"net/http"
+)
+
 func main() {
+
+	var proxy = flag.String("proxy", "localhost:8000", "Proxy dir")
+	var bucketName = flag.String("bucket", "", "S3 bucket name.")
+	var dir = flag.String("directory", "", "Directory name.")
+	flag.Parse()
+
+	if *bucketName == "" {
+		fmt.Println("Missing parameters.")
+		return
+	}
+
+	request := fmt.Sprintf("http://%v/example?bucket=%v&dir=%v", *proxy, *bucketName, *dir)
+	resp, err := http.Get(request)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	scanner := bufio.NewScanner(resp.Body)
+	for i := 0; scanner.Scan(); i++ {
+		fmt.Println(scanner.Text())
+	}
+
 }
--- labs/aws-s3-explorer/awsS3Explorer.go
+++ labs/aws-s3-explorer/awsS3Explorer.go
@@ -1,4 +1,158 @@
 package main
 
+import (
+	"encoding/json"
+	"encoding/xml"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
+type onlyBucketCase struct {
+	BucketName       string
+	ObjectsCount     int
+	DirectoriesCount int
+	Extensions       map[string]int
+}
+
+type bucketAndDirCase struct {
+	BucketName       string
+	DirectoryName    string
+	ObjectsCount     int
+	DirectoriesCount int
+	Extensions       map[string]int
+}
+
+type Content struct {
+	Key string `xml:"Key"`
+}
+
+type XMLResult struct {
+	XMLName  xml.Name  `xml:"ListBucketResult"`
+	Name     string    `xml:"Name"`
+	Contents []Content `xml:"Contents"`
+}
+
+func infoSearching(res http.ResponseWriter, req *http.Request) {
+
+	extensions := make(map[string]int)
+	directories := make(map[string]bool)
+	objects := make(map[string]bool)
+
+	bucketName := req.FormValue("bucket")
+	reqDir := req.FormValue("dir")
+	url := fmt.Sprintf("https://%v.s3.amazonaws.com", bucketName)
+
+	resp, gerr := http.Get(url)
+	if gerr != nil {
+		io.WriteString(res, "Get S3 connection error.")
+		return
+	}
+	defer resp.Body.Close()
+
+	data, rerr := ioutil.ReadAll(resp.Body)
+	if rerr != nil {
+		io.WriteString(res, "Http response reading error.")
+		return
+	}
+
+	var xmlResult XMLResult
+	xerr := xml.Unmarshal(data, &xmlResult)
+	if xerr != nil {
+		io.WriteString(res, "Permission denied\n")
+		return
+	}
+	limitDtree := 3
+	for _, c := range xmlResult.Contents {
+		objKey := c.Key
+		dir := fmt.Sprintf("%v/", reqDir)
+
+		if reqDir != "" && !strings.HasPrefix(objKey, dir) {
+			continue
+		}
+
+		if reqDir != "" && strings.HasPrefix(objKey, dir) {
+			objKey = strings.Replace(objKey, dir, "", 1)
+			if objKey == "" {
+				continue
+			}
+		}
+
+		if strings.Count(objKey, "/") > limitDtree {
+			continue
+		}
+		if strings.HasSuffix(objKey, "/") {
+			if !directories[objKey] {
+				directories[objKey] = true
+			}
+		}
+
+		if strings.Contains(objKey, ".") {
+			_, exists := objects[objKey]
+			if !exists {
+				objects[objKey] = true
+			}
+
+			ext := strings.Split(objKey, ".")
+			_, exists = extensions[ext[len(ext)-1]]
+			if !exists {
+				extensions[ext[len(ext)-1]] = 1
+			}
+			if exists {
+				extensions[ext[len(ext)-1]] += 1
+			}
+
+		}
+	}
+
+	if reqDir == "" {
+		bd := onlyBucketCase{
+			BucketName:       xmlResult.Name,
+			ObjectsCount:     len(objects),
+			DirectoriesCount: len(directories),
+			Extensions:       extensions,
+		}
+
+		out, err := json.MarshalIndent(bd, "", "\t")
+		if err != nil {
+			panic(err)
+		}
+		res.Header().Set("Content-Type", "application/json")
+		res.Write(out)
+	}
+	if reqDir != "" {
+		dd := bucketAndDirCase{
+			BucketName:       xmlResult.Name,
+			DirectoryName:    reqDir,
+			ObjectsCount:     len(objects),
+			DirectoriesCount: len(directories),
+			Extensions:       extensions,
+		}
+
+		out, err := json.MarshalIndent(dd, "", "\t")
+		if err != nil {
+			panic(err)
+		}
+		res.Header().Set("Content-Type", "application/json")
+		res.Write(out)
+	}
+}
 func main() {
+
+	var port = flag.Int("port", 9000, "Port numb.")
+	flag.Parse()
+
+	socket := fmt.Sprintf("localhost:%v", *port)
+	http.HandleFunc("/example", func(res http.ResponseWriter, req *http.Request) {
+		infoSearching(res, req)
+	})
+
+	err := http.ListenAndServe(socket, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
